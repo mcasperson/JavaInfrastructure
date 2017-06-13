a5c8a4750452011267babb414f70c42d1127c04c
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-13 11:17:07 +1000

    Still trying to get sql and OD servers to install

--- roles/octopusdeploy/templates/OctopusDeploy.ps1
+++ roles/octopusdeploy/templates/OctopusDeploy.ps1
@@ -1,8 +1,8 @@
 #jinja2: newline_sequence:'\r\n'
+
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "create-instance --instance `"OctopusServer`" --config `"C:\Octopus\OctopusServer.config`"" -Wait
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "database --instance `"OctopusServer`" --connectionString `"Data Source=(local)\SQLEXPRESS;Initial Catalog=OctopusDeploy;Integrated Security=True`" --create --grant `"NT AUTHORITY\SYSTEM`"" -Wait
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "configure --instance `"OctopusServer`" --upgradeCheck `"True`" --upgradeCheckWithStatistics `"True`" --webAuthenticationMode `"UsernamePassword`" --webForceSSL `"False`" --webListenPrefixes `"http://localhost:80/`" --commsListenPort `"10943`" --serverNodeName `"OctopusDeploy`"" -Wait
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "service --instance `"OctopusServer`" --stop" -Wait
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "admin --instance `"OctopusServer`" --username `"admin`" --email `"a@a.com`" --password `"Password01!`"" -Wait
 Start-Process -FilePath "C:\Program Files\Octopus Deploy\Octopus\Octopus.Server.exe" -ArgumentList "service --instance `"OctopusServer`" --install --reconfigure --start --dependOn `"MSSQL`$SQLEXPRESS`"" -Wait
-
