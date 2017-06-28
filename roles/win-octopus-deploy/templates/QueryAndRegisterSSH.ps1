012424450e877f01b79fb07bb0135c5f2ecae0fb
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-29 08:43:10 +1000

    Query and register scripts now call each other with parameters

--- roles/win-octopus-deploy/templates/QueryAndRegisterSSH.ps1
+++ roles/win-octopus-deploy/templates/QueryAndRegisterSSH.ps1
@@ -1,5 +1,10 @@
-$instances = & aws ec2 describe-instances `
+$instances = & "C:\Program Files\Amazon\AWSCLI\aws.exe" ec2 describe-instances `
     --region us-east-1 `
     --filters "Name=tag:OS,Values=centos" "Name=tag:Purpose,Values=DukeLegion" `
     --query "Reservations[*].Instances[*].PublicIpAddress" `
     --output=text
+
+ForEach ($host in $($instances -split "`r`n"))
+{
+    ./RegisterSSH.ps1 -Host $host -Account "Centos Duke Legion"
+}
--- roles/win-octopus-deploy/templates/RegisterSSH.ps1
+++ roles/win-octopus-deploy/templates/RegisterSSH.ps1
@@ -1,12 +1,15 @@
+param([String]$Host)
+param([String]$Account)
+
 $headers = @{"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}"}
 
 $environments = Invoke-RestMethod "http://localhost/api/environments/all" -Headers $headers -Method Get
 $theEnvironment = $environments | ? { $_.Name -eq "DukeLegion" }
 
 $accounts = Invoke-RestMethod "http://url.to.octopus/api/accounts/all" -Headers $headers -Method Get
-$theAccount = $accounts | ? { $_.Name -eq "Centos Duke Legion" }
+$theAccount = $accounts | ? { $_.Name -eq "$Account" }
 
-$discovered = Invoke-RestMethod "http://localhost/api/machines/discover?host=hostnameoripaddress&type=Ssh" -Headers $headers -Method Get
+$discovered = Invoke-RestMethod "http://localhost/api/machines/discover?host=$Host&type=Ssh" -Headers $headers -Method Get
 
 #$discovered.Name = "MySshTargetName" # If you wanted to change the name of the deployment target (default is host name)
 $discovered.Roles += "MyRole"
