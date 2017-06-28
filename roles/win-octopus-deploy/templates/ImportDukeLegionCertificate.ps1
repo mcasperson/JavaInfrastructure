7380a366a4a6ec5186ec9d0bdcff9a1ebbe4fb00
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-29 07:45:29 +1000

    Installed winrm on Ansible worker node. Also started work on scripting the creation of SSH accounts

---
+++ roles/win-octopus-deploy/templates/ImportDukeLegionCertificate.ps1
@@ -0,0 +1,3 @@
+$centosSSHAccountDetails = '{"AccountType": "SshKeyPair", "Username": "centos", "PrivateKeyFile": {"NewValue": "C:\Apps\DukeLegion.pem"},"Name": "Centos Duke Legion","Description": "Centos Duke Legion Account"}'
+$headers = @{"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}", "Content-Type"="Content-Type:application/json"}
+$environments = Invoke-RestMethod "http://localhost/api/accounts" -Headers $headers -Method Post -Body $centosSSHAccountDetails
---
+++ roles/win-octopus-deploy/templates/RegisterSSH.ps1
@@ -0,0 +1,15 @@
+$headers = @{"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}"}
+$environments = Invoke-RestMethod "http://localhost/api/environments/all" -Headers $headers -Method Get
+$theEnvironment = $environments | ? { $_.Name -eq "DukeLegion" }
+$accounts = Invoke-RestMethod "http://url.to.octopus/api/accounts/all" -Headers $headers -Method Get
+$theAccount = $accounts | ? { $_.Name -eq "TheAccount" }
+$discovered = Invoke-RestMethod "http://localhost/api/machines/discover?host=hostnameoripaddress&type=Ssh" -Headers $headers -Method Get
+
+#$discovered.Name = "MySshTargetName" # If you wanted to change the name of the deployment target (default is host name)
+$discovered.Roles += "MyRole"
+$discovered.EnvironmentIds += $theEnvironment.Id
+$discovered.Endpoint.AccountId = $theAccount.Id
+
+$discovered | ConvertTo-Json -Depth 10
+
+Invoke-RestMethod "http://localhost/api/machines" -Headers $headers -Method Post -Body ($discovered | ConvertTo-Json -Depth 10)
