bc737ef5c44487e5bc528c081de55f406dc3e350
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-29 08:01:15 +1000

    Fixed up SSH account creation script

--- roles/win-octopus-deploy/templates/ImportDukeLegionCertificate.ps1
+++ roles/win-octopus-deploy/templates/ImportDukeLegionCertificate.ps1
@@ -1,3 +1,15 @@
-$centosSSHAccountDetails = '{"AccountType": "SshKeyPair", "Username": "centos", "PrivateKeyFile": {"NewValue": "C:\Apps\DukeLegion.pem"},"Name": "Centos Duke Legion","Description": "Centos Duke Legion Account"}'
-$headers = @{"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}", "Content-Type"="Content-Type:application/json"}
-$environments = Invoke-RestMethod "http://localhost/api/accounts" -Headers $headers -Method Post -Body $centosSSHAccountDetails
+$centosSSHAccountDetails = @{
+	"AccountType"="SshKeyPair";
+	"Username"="centos";
+	"PrivateKeyFile"=@{"NewValue"="C:\Apps\DukeLegion.pem"};
+	"Name"="Centos Duke Legion";
+	"Description"="Centos Duke Legion Account"
+}
+$headers = @{
+	"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}";
+	"Content-Type"="application/json"
+}
+$addAccount = Invoke-RestMethod "http://localhost/api/accounts" `
+    -Headers $headers `
+    -Method Post `
+    -Body ($centosSSHAccountDetails  | ConvertTo-Json -Depth 10)
