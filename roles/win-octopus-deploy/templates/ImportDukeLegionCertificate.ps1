bef285de93d5327cfc754c8108e975c62685e6a3
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-29 10:37:03 +1000

    Added the ability to register ubuntu hosts

--- roles/win-octopus-deploy/templates/ImportDukeLegionCertificate.ps1
+++ roles/win-octopus-deploy/templates/ImportDukeLegionCertificate.ps1
@@ -1,3 +1,8 @@
+$headers = @{
+	"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}";
+	"Content-Type"="application/json"
+}
+
 $centosSSHAccountDetails = @{
 	"AccountType"="SshKeyPair";
 	"Username"="centos";
@@ -11,10 +16,26 @@ $centosSSHAccountDetails = @{
 	"Name"="Centos Duke Legion";
 	"Description"="Centos Duke Legion Account"
 }
-$headers = @{
-	"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}";
-	"Content-Type"="application/json"
+
+$addAccount = Invoke-RestMethod "http://localhost/api/accounts" `
+    -Headers $headers `
+    -Method Post `
+    -Body ($centosSSHAccountDetails  | ConvertTo-Json -Depth 10)
+
+$centosSSHAccountDetails = @{
+	"AccountType"="SshKeyPair";
+	"Username"="ubuntu";
+	"PrivateKeyFile"=@{
+	    "HasValue"="true";
+	    "NewValue"="{{duke_legion_pem}}";
+    };
+    "PrivateKeyPassphrase"=@{
+        "HasVaule"="false";
+    };
+	"Name"="Ubuntu Duke Legion";
+	"Description"="Ubuntu Duke Legion Account"
 }
+
 $addAccount = Invoke-RestMethod "http://localhost/api/accounts" `
     -Headers $headers `
     -Method Post `
--- roles/win-octopus-deploy/templates/QueryAndRegisterSSH.ps1
+++ roles/win-octopus-deploy/templates/QueryAndRegisterSSH.ps1
@@ -8,3 +8,14 @@ foreach ($line in $instances.Split("`n"))
 {
     ./RegisterSSH.ps1 -LinuxHost $line -Account "Centos Duke Legion"
 }
+
+$instances = & "C:\Program Files\Amazon\AWSCLI\aws.exe" ec2 describe-instances `
+    --region us-east-1 `
+    --filters "Name=tag:OS,Values=ubuntu" "Name=tag:Purpose,Values=DukeLegion" `
+    --query "Reservations[*].Instances[*].PublicIpAddress" `
+    --output=text
+
+foreach ($line in $instances.Split("`n"))
+{
+    ./RegisterSSH.ps1 -LinuxHost $line -Account "Ubuntu Duke Legion"
+}
