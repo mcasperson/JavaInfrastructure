9fafd2b71516a1b4a256c708b733a4d422088de0
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-23 12:12:22 +1000

    Made script more generic

--- roles/win-octopus-deploy/templates/RegisterSSH.ps1
+++ roles/win-octopus-deploy/templates/RegisterSSH.ps1
@@ -3,9 +3,11 @@ param(
 	[String]$account
 )
 
+$hostName="http://localhost"
+
 $headers = @{"X-Octopus-ApiKey"="{{api_key.stdout_lines[0]}}"}
 
-$existing = Invoke-RestMethod "http://localhost/api/machines/all" -Headers $headers -Method Get
+$existing = Invoke-RestMethod "$hostName/api/machines/all" -Headers $headers -Method Get
 
 foreach ($machine in $existing) {
 	if ($machine.Endpoint.Host -eq $linuxHost) {
@@ -14,19 +16,19 @@ foreach ($machine in $existing) {
 	}
 }
 
-$environments = Invoke-RestMethod "http://localhost/api/environments/all" -Headers $headers -Method Get
+$environments = Invoke-RestMethod "$hostName/api/environments/all" -Headers $headers -Method Get
 $theEnvironment = $environments | ? { $_.Name -eq "DukeLegion" }
 
-$accounts = Invoke-RestMethod "http://localhost/api/accounts/all" -Headers $headers -Method Get
+$accounts = Invoke-RestMethod "$hostName/api/accounts/all" -Headers $headers -Method Get
 $theAccount = $accounts | ? { $_.Name -eq "$account" }
 
-$discovered = Invoke-RestMethod "http://localhost/api/machines/discover?host=$linuxHost&type=Ssh" -Headers $headers -Method Get
+$discovered = Invoke-RestMethod "$hostName/api/machines/discover?host=$linuxHost&type=Ssh" -Headers $headers -Method Get
 
 #$discovered.Name = "MySshTargetName" # If you wanted to change the name of the deployment target (default is host name)
 $discovered.Roles += "DukeLegion"
 $discovered.EnvironmentIds += $theEnvironment.Id
 $discovered.Endpoint.AccountId = $theAccount.Id
 
-Invoke-RestMethod "http://localhost/api/machines" -Headers $headers -Method Post -Body ($discovered | ConvertTo-Json -Depth 10)
+Invoke-RestMethod "$hostName/api/machines" -Headers $headers -Method Post -Body ($discovered | ConvertTo-Json -Depth 10)
 
 Write-Host "Host $linuxHost added under account $account"
