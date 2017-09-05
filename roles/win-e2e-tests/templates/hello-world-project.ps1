96dfc3e3c69d3e38c3c411086344a7b75b8c950d
Matthew Casperson <matthewcasperson@gmail.com>
2017-09-06 07:46:56 +1000

    Working on adding new servers to linux builds

--- roles/win-e2e-tests/templates/hello-world-project.ps1
+++ roles/win-e2e-tests/templates/hello-world-project.ps1
@@ -1,3 +1,3 @@
 #jinja2: newline_sequence:'\r\n'
 cd C:\E2ETests
-& C:\Java\x86\jre1.8.0_131\bin\java.exe "-DscreenZoomFactor=1.0" "-DmoveCursorToElement=true" "-DfeatureGroupName=local" "-Dconfiguration=config.xml" "-Ddataset=dataset.xml" "-DmoveCursorToElement=true" "-DsaveReportsInHomeDir=true" "-DstartInternalProxy=-browserMob" "-DtestSource=hello-world-project.feature" "-DtestDestination=CHROMEFULLSCREEN" -jar C:\Apps\IridiumApplicationTesting.jar
+& C:\Java\x86\jre1.8.0_131\bin\java.exe "-DmouseMoveVerticalOffset=40" "-DscreenZoomFactor=1.0" "-DmoveCursorToElement=true" "-DfeatureGroupName=local" "-Dconfiguration=config.xml" "-Ddataset=dataset.xml" "-DmoveCursorToElement=true" "-DsaveReportsInHomeDir=true" "-DstartInternalProxy=-browserMob" "-DtestSource=hello-world-project.feature" "-DtestDestination=CHROMEFULLSCREEN" -jar C:\Apps\IridiumApplicationTesting.jar
