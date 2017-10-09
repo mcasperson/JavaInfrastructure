9c0829658b48d516779e3e0f45b6c009283ee80a
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-09 15:17:12 +1000

    Trying to get APR on centos

---
+++ roles/yum-linux-common/templates/aprbuild.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+cd /tmp
+wget http://apache.mirror.amaze.com.au/apr/apr-1.6.2.tar.gz
+tar -zxvf apr-1.6.2.tar.gz
+cd apr-1.6.2
+./configure
+make
+make install
+
+cd /tmp
+wget http://apache.mirror.amaze.com.au/apr/apr-util-1.6.0.tar.gz
+tar -zxvf apr-util-1.6.0.tar.gz
+cd apr-util-1.6.0
+./configure --with-apr=/usr/local/apr
+make
+make install
+
+cd /tmp
+wget http://apache.melbourneitmirror.net/tomcat/tomcat-connectors/native/1.2.14/source/tomcat-native-1.2.14-src.tar.gz
+tar -zxvf tomcat-native-1.2.14-src.tar.gz
+cd tomcat-native-1.2.14-src/native
+./configure --with-apr=/usr/local/apr --with-java-home=/usr/lib/jvm/java-1.8.0-openjdk
+make
+make install
