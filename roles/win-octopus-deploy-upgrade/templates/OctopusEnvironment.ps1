adbf6624bc2db324fe7e12f781dd48200be127f7
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-16 11:31:54 +1000

    Added retry for choco commands

--- roles/win-octopus-deploy-upgrade/templates/OctopusEnvironment.ps1
+++
@@ -1,4 +0,0 @@
-#jinja2: newline_sequence:'\r\n'
-
-Start-Process -FilePath "C:\ProgramData\chocolatey\bin\Octo.exe" -ArgumentList "create-environment --name=local --ignoreIfExists --server=http://localhost --apiKey={{api_key.stdout_lines[0]}}" -Wait -NoNewWindow
-Start-Process -FilePath "C:\ProgramData\chocolatey\bin\Octo.exe" -ArgumentList "create-environment --name=DukeLegion --ignoreIfExists --server=http://localhost --apiKey={{api_key.stdout_lines[0]}}" -Wait -NoNewWindow
--- roles/win-octopus-deploy-upgrade/templates/SQLInstall.ps1
+++
@@ -1,4 +0,0 @@
-#jinja2: newline_sequence:'\r\n'
-
-$process = Start-Process -FilePath "C:\Apps\SqlServerExpress\Setup.exe" -ArgumentList "/Q /UPDATEENABLED=False /ACTION=INSTALL /FEATURES=SQLENGINE /INSTANCENAME=`"{{sql_instance_name}}`" /INSTANCEID=`"{{sql_instance_name}}`" /IACCEPTSQLSERVERLICENSETERMS" -Wait -NoNewWindow -PassThru -RedirectStandardOutput "C:\Apps\SqlInstall.out" -RedirectStandardError "C:\Apps\SQLInstall.err"
-exit $process.ExitCode
