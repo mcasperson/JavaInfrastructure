f28e9e1beae6c3dc562d59fe3025c3cb30432562
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-10 09:12:53 +1000

    Fixing up APR build script

--- roles/yum-linux-common/templates/aprbuild.sh
+++ roles/yum-linux-common/templates/aprbuild.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 cd /tmp
-wget http://apache.mirror.amaze.com.au/apr/apr-1.6.2.tar.gz
-tar -zxvf apr-1.6.2.tar.gz
-cd apr-1.6.2
+wget http://apache.mirror.amaze.com.au/apr/apr-1.6.3.tar.gz
+tar -zxvf apr-1.6.3.tar.gz
+cd apr-1.6.3
 ./configure
 make
 make install
 
 cd /tmp
-wget http://apache.mirror.amaze.com.au/apr/apr-util-1.6.0.tar.gz
-tar -zxvf apr-util-1.6.0.tar.gz
-cd apr-util-1.6.0
+wget http://apache.mirror.amaze.com.au/apr/apr-util-1.6.1.tar.gz
+tar -zxvf apr-util-1.6.1.tar.gz
+cd apr-util-1.6.1
 ./configure --with-apr=/usr/local/apr
 make
 make install
