d966bee88512e2eb82823dc6810f9fa2d6805481
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-13 19:00:33 +1000

    Fixing up schedule tags, and still trying to size the nexus instance

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm ../JavaAnsible*.tar.gz
-tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.51.tar.gz .
+tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.54.tar.gz .
