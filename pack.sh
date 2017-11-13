1dc4245582a8d85434d0ede03e67792fba1855d9
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-13 17:26:42 +1000

    Added a retry to the nexus download

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm ../JavaAnsible*.tar.gz
-tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.49.tar.gz .
+tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.51.tar.gz .
