7c84e5311bea05b86165dbe1c6c64eba6ca5118b
Matthew Casperson <matthewcasperson@gmail.com>
2018-01-10 06:52:40 +1000

    Converted all files to unix line endings

--- pack.sh
+++ pack.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm ../JavaAnsible*.tar.gz
-tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.69.tar.gz *
+find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs -d '\n' -L 1 dos2unix -k
+tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.71.tar.gz *
