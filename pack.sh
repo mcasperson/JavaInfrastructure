803ce1499cc2b9683660a76c18a983b6ef37cf4c
Matthew Casperson <matthewcasperson@gmail.com>
2018-01-10 07:05:00 +1000

    Fixed up encrypted file, and added git configuration to retain unix line endings

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm ../JavaAnsible*.tar.gz
+# This command will convert all non-binary fines to the linux line endings
 find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs -d '\n' -L 1 dos2unix -k
 tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.71.tar.gz *
