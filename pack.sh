24287c1e30744234fb16c921e50a074ed77a2ab3
Matthew Casperson <matthewcasperson@gmail.com>
2018-02-11 11:57:18 +1000

    Fixed up pack script to work on a mac

--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm ../JavaAnsible*.tar.gz
 # This command will convert all non-binary fines to the linux line endings
-find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs -d '\n' -L 1 dos2unix -k
-tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.71.tar.gz *
+find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs -n1 '\n' -L 1 dos2unix -k
+tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.72.tar.gz *
