7db2900eb4e242b5ecf30a737286717cf629d904
Matthew Casperson <matthewcasperson@gmail.com>
2018-02-11 12:05:30 +1000

    Fixed up version

--- pack.sh
+++ pack.sh
@@ -2,4 +2,4 @@
 rm ../JavaAnsible*.tar.gz
 # This command will convert all non-binary fines to the linux line endings
 find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs -n1 '\n' -L 1 dos2unix -k
-tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.0.72.tar.gz *
+tar -cz --exclude='.git' --exclude='.idea' -f ../JavaAnsible.0.0.72.tar.gz *
